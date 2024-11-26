509766f549ea3cae8e086dc11f43a7a2a3b1b566
klawisha012 <klawisha012@gmail.com>
2024-11-26 23:17:43 +0300

    2 threads

--- cheat/cheat.cpp
+++ cheat/cheat.cpp
@@ -36,6 +36,40 @@ bool WorldToScreen(const Vec3& worldPos, Vec2& screenPos, float* viewMatrix, int
 	return true; // Объект видим
 }
 
+void monitorPlayers(ProcessMemory& pm, DWORD playerCount, DWORD entityList) {
+	while (true) {
+		// Получаем количество игроков
+		playerCount = pm.ReadMemory<DWORD>(offsets::PlayerCount);
+		std::cout << "Количество игроков: " << playerCount << std::endl;
+
+		// Считываем данные каждого игрока
+		for (DWORD i = 0; i < playerCount; i++) {
+			DWORD entityAddress = pm.ReadMemory<DWORD>(entityList + i * 0x4); // Каждый игрок занимает 0x4
+			if (!entityAddress) {
+				continue; // Если сущность не существует, пропускаем
+			}
+
+			// Чтение координат игрока
+			float posX = pm.ReadMemory<float>(entityAddress + offsets::PositionX);
+			float posY = pm.ReadMemory<float>(entityAddress + offsets::PositionY);
+			float posZ = pm.ReadMemory<float>(entityAddress + offsets::PositionZ);
+			// Выводим координаты игрока
+			std::cout << "Игрок " << i + 1 << ": "
+				<< "X: " << posX << ", "
+				<< "Y: " << posY << ", "
+				<< "Z: " << posZ << std::endl;
+		}
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+	}
+}
+
+void updateHealth(ProcessMemory& pm, DWORD lp) {
+	while (true) {
+		pm.WriteMemory<DWORD>(offsets::HealthValue + lp, 1000);
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+	}
+}
+
 int main()
 {
 	setlocale(LC_ALL, "");
@@ -53,6 +87,7 @@ int main()
 	else {
 		std::cerr << "Не удалось получить PID!" << std::endl;
 	}
+	
 	HANDLE processHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, processId);
 	if (processHandle == nullptr || processHandle == INVALID_HANDLE_VALUE) {
 		std::cerr << "Не удалось открыть процесс. Код ошибки: " << GetLastError() << std::endl;
@@ -65,33 +100,9 @@ int main()
 	DWORD cp = pm.ReadMemory<DWORD>(offsets::PlayerCount);
 	DWORD entityList = pm.ReadMemory<DWORD>(offsets::EntityList);
 
-	while (true) {
-		// Получаем количество игроков
-		DWORD playerCount = pm.ReadMemory<DWORD>(offsets::PlayerCount);
-		std::cout << "Количество игроков: " << playerCount << std::endl;
+	std::thread playerThread(monitorPlayers, std::ref(pm), cp, entityList);
+	std::thread healthThread(updateHealth, std::ref(pm), lp);
+	playerThread.join();
+	healthThread.join();
 
-		// Считываем данные каждого игрока
-		for (DWORD i = 0; i < playerCount; i++) {
-			DWORD entityAddress = pm.ReadMemory<DWORD>(entityList + i * 0x4); // Каждый игрок занимает 0x4
-			if (!entityAddress) {
-				continue; // Если сущность не существует, пропускаем
-			}
-
-			// Чтение координат игрока
-			float posX = pm.ReadMemory<float>(entityAddress + offsets::PositionX);
-			float posY = pm.ReadMemory<float>(entityAddress + offsets::PositionY);
-			float posZ = pm.ReadMemory<float>(entityAddress + offsets::PositionZ);
-			// Выводим координаты игрока
-			std::cout << "Игрок " << i + 1<< ": "
-				<< "X: " << posX << ", "
-				<< "Y: " << posY << ", "
-				<< "Z: " << posZ << std::endl;
-		}
-		std::this_thread::sleep_for(std::chrono::milliseconds(100));
-	}
-	while (true)
-	{
-		pm.WriteMemory<DWORD>(offsets::HealthValue + lp, 1000);
-		std::this_thread::sleep_for(std::chrono::milliseconds(100));
-	}
 }
